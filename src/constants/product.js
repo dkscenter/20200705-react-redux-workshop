2ed14f025221b37613079b9cefb829fd8ab0dca4
คมเพชร มีทรัพย์ <komphet@Macintosh.local>
2020-06-20 06:37:49 +0700

    Update Data

---
+++ src/constants/product.js
@@ -0,0 +1,115 @@
+export default [
+    {
+        categoryName: "Soup & Salad",
+        categoryNumber: "01",
+        categoryImage: "/images/01.jpg",
+        products: [
+            {
+                productName: "CAESAR SALAD",
+                productNumber: "01",
+                productImage: "/images/01/01.webp"
+            },
+            {
+                productName: "CRAB CHOWDER",
+                productNumber: "02",
+                productImage: "/images/01/02.webp"
+            },
+            {
+                productName: "PARMA HAM SALAD",
+                productNumber: "03",
+                productImage: "/images/01/03.webp"
+            },
+            {
+                productName: "SPICY TUNA SALAD",
+                productNumber: "04",
+                productImage: "/images/01/04.webp"
+            },
+            {
+                productName: "ITALIAN SAUSAGE AND ROCKET",
+                productNumber: "05",
+                productImage: "/images/01/05.webp"
+            }
+        ]
+    },
+    {
+        categoryName: "Beer",
+        categoryNumber: "02",
+        categoryImage: "/images/02.jpg",
+        products: [
+            {
+                productName: "MAISEL'S WEISSE ORIGINAL",
+                productNumber: "01",
+                productImage: "/images/02/01.webp"
+            },
+            {
+                productName: "KONA BIG WAVE",
+                productNumber: "02",
+                productImage: "/images/02/02.webp"
+            },
+        ]
+    },
+    {
+        categoryName: "Pizza",
+        categoryNumber: "03",
+        categoryImage: "/images/03.jpg",
+        products: [
+            {
+                productName: "PROCUITTO E FUNGHI",
+                productNumber: "01",
+                productImage: "/images/03/01.webp"
+            },
+            {
+                productName: "PESCATORA",
+                productNumber: "02",
+                productImage: "/images/03/02.webp"
+            },
+            {
+                productName: "4 SEASONS",
+                productNumber: "03",
+                productImage: "/images/03/03.webp"
+            },
+            {
+                productName: "STAGIONI",
+                productNumber: "04",
+                productImage: "/images/03/04.webp"
+            },
+            {
+                productName: "SPINACH & SALMON",
+                productNumber: "05",
+                productImage: "/images/03/05.webp"
+            }
+        ]
+    },
+    {
+        categoryName: "Pasta",
+        categoryNumber: "04",
+        categoryImage: "/images/04.jpg",
+        products: [
+            {
+                productName: "CLAMS, GARLIC, CHILI",
+                productNumber: "01",
+                productImage: "/images/04/01.webp"
+            },
+            {
+                productName: "TOM YUM",
+                productNumber: "02",
+                productImage: "/images/04/02.webp"
+            },
+            {
+                productName: "CABONARA",
+                productNumber: "03",
+                productImage: "/images/04/03.webp"
+            },
+            {
+                productName: "CRAB MEAT, GARLIC, CHILI",
+                productNumber: "04",
+                productImage: "/images/04/04.webp"
+            },
+            {
+                productName: "ITALIAN SAUSAGE, GARLIC, CHILI",
+                productNumber: "05",
+                productImage: "/images/04/05.webp"
+            }
+        ]
+    },
+]
