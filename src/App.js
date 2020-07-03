ad2fd377f6429289f5bb3fd41416ef44e27c9f36
Komphet <komphet.me@gmail.com>
2020-07-04 03:28:47 +0700

    Update Workshop Step - 1

--- src/App.js
+++ src/App.js
@@ -1,154 +1,85 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBarComponent from "./components/NavBarComponent";
-import CategoryListComponent from "./components/CategoryListComponent";
-import ProductListComponent from "./components/ProductListComponent";
-import ProductItemComponent from "./components/ProductItemComponent";
-import CartComponent from "./components/CartComponent";
-import { StoreContext, store } from "./context/store-context";
-import AddressComponent from "./components/AddressComponent";
-import { LocationContext, location } from "./context/location-context";
+import Sidebar from "react-sidebar";
+import { Button } from "reactstrap";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // State also contains the updater function so it will
-    // be passed down into the context provider
-    this.state = {
-      store: store,
-      withComma: (x) => {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      },
-      toggleCart: () => {
-        this.setState((state) => ({
-          store: {
-            ...state.store,
-            cart: {
-              ...state.store.cart,
-              isOpen: state.store.cart.isOpen ? false : true,
-            },
-          },
-        }));
-      },
-      addCartAmount: (index, operator) => {
-        let { products } = this.state.store.cart;
-        products[index].amount =
-          operator === "plus"
-            ? products[index].amount + 1
-            : products[index].amount - 1;
-
-        if (products[index].amount <= 0) {
-          products.splice(index, 1);
-          this.__animateCart();
-        }
-
-        localStorage.setItem("cart", JSON.stringify(products));
-
-        this.setState((state) => ({
-          store: {
-            ...state.store,
-            cart: {
-              ...state.store.cart,
-              products,
-            },
-          },
-        }));
-      },
-      addCart: (newProduct) => {
-        let { products } = this.state.store.cart;
-        let findProduct = products.findIndex(
-          (product) => product.productNumber === newProduct.productNumber
-        );
-        if (findProduct >= 0) {
-          products[findProduct].amount = products[findProduct].amount + 1;
-        } else {
-          newProduct.amount = 1;
-          products.push(newProduct);
-        }
-
-        localStorage.setItem("cart", JSON.stringify(products));
-        this.__animateCart();
-
-        this.setState((state) => ({
-          store: {
-            ...state.store,
-            cart: {
-              ...state.store.cart,
-              products,
-            },
-          },
-        }));
-      },
-      location: {
-        address: location.address,
-        setAddress: (e) => {
-          this.setState({
-            location: {
-              ...this.state.location,
-              address: {
-                ...this.state.location.address,
-                [e.target.name]: e.target.value,
-              },
-            },
-          });
-        },
-      },
-    };
-  }
-
-  __animateCart = ()=>{
-    this.setState((state) => ({
-      store: {
-        ...state.store,
-        cart: {
-          ...state.store.cart,
-          isAnimate: true,
-        },
-      },
-    }));
-    setTimeout(() => {
-      this.setState((state) => ({
-        store: {
-          ...state.store,
-          cart: {
-            ...state.store.cart,
-            isAnimate: false,
-          },
-        },
-      }));
-    }, 1000);
-  }
-
   render() {
     return (
-      <Router>
-        <LocationContext.Provider value={this.state.location}>
-          <StoreContext.Provider value={this.state}>
-            <div className="delivery">
-              <NavBarComponent />
-              <CartComponent>
-                {/* <CategoryList/> */}
-                <Switch>
-                  <Route path="/address">
-                    <AddressComponent />
-                  </Route>
-                  <Route path="/category/:categoryId/product/:productId">
-                    <ProductItemComponent />
-                  </Route>
-                  <Route path="/category/:id">
-                    <ProductListComponent />
-                  </Route>
-                  <Route path="/">
-                    <CategoryListComponent />
-                  </Route>
-                </Switch>
-              </CartComponent>
+      <div className="delivery">
+        <div className="navbar">
+          <div className="nav-title">
+            <a href="/">Delivery Workshop</a>
+          </div>
+          <div className="nav-menu">
+            <button className="btn text-light cart-total-amount-btn">
+              <FontAwesomeIcon icon={["fas", "shopping-cart"]} />
+              <div className="cart-total-amount bg-danger">5</div>
+            </button>
+            <button className="btn text-light">
+              <FontAwesomeIcon icon={["fas", "user-circle"]} />
+            </button>
+          </div>
+        </div>
+        <Sidebar
+          sidebar={
+            <div className="sidebar">
+              <div className="cart-item">
+                <div className="product-name">฿280 PARMA HAM SALAD</div>
+                <div className="cart-amount">
+                  <button className="cart-minus">
+                    <FontAwesomeIcon icon={["fas", "minus"]} />
+                  </button>
+                  <div className="cart-amount-value">1</div>
+                  <button className="cart-plus">
+                    <FontAwesomeIcon icon={["fas", "plus"]} />
+                  </button>
+                </div>
+              </div>
+              <hr />
+              Total Price: ฿280
+              <Button className="cart-checkout" size="sm" color="secondary">
+                Checkout
+              </Button>
             </div>
-          </StoreContext.Provider>
-        </LocationContext.Provider>
-      </Router>
+          }
+          open={false}
+          onSetOpen={() => {}}
+          styles={{
+            sidebar: {
+              background: "rgb(43, 59, 75)",
+              position: "fixed",
+              minWidth: 300,
+              color: "#fff",
+              zIndex: "99",
+            },
+          }}
+          pullRight={true}
+        >
+          <div className="delivery-container">
+            <div className="category-list">
+              <div className="location">
+                <div className="location-text">Click to set Address</div>
+                <div className="location-edit">
+                  <FontAwesomeIcon icon={["fas", "edit"]} />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12 col-md-6">
+                  <div className="category-box">
+                    <div className="category-img">
+                      <img src={"/images/01.jpg"} alt="Soup" />
+                    </div>
+                    <div className="category-detail">
+                      <p className="d-font">Hello</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Sidebar>
+      </div>
     );
   }
 }
